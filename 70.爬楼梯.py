a4ba198e7972cff82bdfe6fa8780998a64d62b9a
jeremy-feng <jeremy-feng@qq.com>
2023-04-01 08:49:04 +0800

    :white_check_mark: [70] 爬楼梯

--- 70.爬楼梯.py
+++ 70.爬楼梯.py
@@ -4,17 +4,28 @@
 # [70] 爬楼梯
 #
 
+
 # @lc code=start
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
+        # if n <= 2:
+        #     return n
+        # 递归
         # return self.climbStairs(n - 1) + self.climbStairs(n - 2)
-        a, b, c = 1, 2, 3
-        for _ in range(3, n):
-            a, b = b, c
-            c = a + b
-        return c
+        # 迭代
+        # a, b, c = 1, 2, 3
+        # for _ in range(3, n):
+        #     a, b = b, c
+        #     c = a + b
+        # return c
+        # 完全背包，背包容量为 n
+        dp = [0 for j in range(n + 1)]
+        dp[0] = 1
+        for j in range(n + 1):
+            for i in range(1, 2 + 1):
+                if i <= j:
+                    dp[j] += dp[j - i]
+        return dp[n]
 
 
 # @lc code=end
